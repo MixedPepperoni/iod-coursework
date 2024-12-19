3df36a2ae0de2ff7b7ab940c91bdefe7c893b069
MixedPepperoni <Mixyboo@gmail.com>
2024-12-18 20:57:03 -0500

    Post them

---
+++ Module1/lab6.js
@@ -0,0 +1,74 @@
+const book = {
+  title: "Mybookhere", //works but a bracket forms aroun it
+  desc: "How I fought your mother.", // works but a bracke forms around it
+  author: "Jackie", //works now, I was missing the , like error 1005 said.
+  page: "256", // Rewatch first hour of class 12/17/24 to figure out errors. GOes vvoer lab5. makesure to rename lab 6.
+};
+
+console.log(book.title);
+console.log(book.desc);
+console.log(book.author);
+console.log(book.page);
+
+book.desc = "I married your mother"; // Works to change output.
+///test output to get indivtuals. I think bracket error is me msisireading. Seems fine in terminal.
+console.log(book); // Console logging whole book, good. Here, others are doing it indivdually. Remember that.
+
+const mathbook = {
+    title: "Math", 
+    desc: "A book on math",
+    author: "Yu Taro", 
+    page: "356", 
+  };
+  
+  console.log(mathbook.title);
+  console.log(mathbook.desc);
+  console.log(mathbook.author);
+  console.log(mathbook.page);
+
+  const hisbook = {
+    title: "History", 
+    desc: "A book on history of Mexico",
+    author: "Gabe Manrow", 
+    page: "3536", 
+  };
+  
+  console.log(hisbook.title);
+  console.log(hisbook.desc);
+  console.log(hisbook.author);
+  console.log(hisbook.page);
+
+  const cookbook = {
+    title: "cook at home", 
+    desc: "A book on making food",
+    author: "Taro Chicken", 
+    page: "188", 
+  };
+  
+  console.log(cookbook.title);
+  console.log(cookbook.desc);
+  console.log(cookbook.author);
+  console.log(cookbook.page);
+
+  const gymbook = {
+    title: "Book detailing body treatment", 
+    desc: "Exercise for a better life.",
+    author: "Him Bro", 
+    page: "234", 
+  };
+  
+  console.log(gymbook.title);
+  console.log(gymbook.desc);
+  console.log(gymbook.author);
+  console.log(gymbook.page);
+  const chembook = {
+    title: "The science of drugs", 
+    desc: "A book on cooking meth ",
+    author: "Yu Ta", 
+    page: "766", 
+  };
+  
+  console.log(chembook.title);
+  console.log(chembook.desc);
+  console.log(chembook.author);
+  console.log(chembook.page);
