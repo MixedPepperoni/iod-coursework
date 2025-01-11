f985f86a868a796d6fbd603d6fe0030bab94ad67
MixedPepperoni <Mixyboo@gmail.com>
2025-01-11 17:20:02 -0500

    uploading exercise 7

--- Module1/LabExercises/exercise7.js
+++ Module1/LabExercises/exercise7.js
@@ -1,25 +1,36 @@
 //Returns the sum of two numbers.
-if (sum(5, 1) != 6) { error }
 function sum(a, b) {
   return a + b;
 }
+//test
+if (sum(5, 1) != 6) { error }
+//return the code
 console.log(sum(5, 1));
+
 //returns the subraction of two numbbers.
 function sub(a, b) {
   return a - b;
 }
+if (sub(5, 1) != 4) { error }
+//return the code
 console.log(sub(5, 1));
+
 //outputs the multipication of two numbers. EX: a*b=answer/2*2=4.
 function mult(a, b) {
   return a * b;
 }
+//test
+if (mult(5, 1) != 5) { error }
+//return the code
 console.log(mult(5, 1));
+
 //returns the division of the two nuumbers. EX; a/b=answer or 9/3=3.
 function divd(a, b) {
   return a / b;
 }
 console.log(divd(5, 2));
-//outputs a phrase in the terminal. "Hello brendan" should be the output.
+
+//outputs a phrase in the terminal. "Hello Brendan" should be the output.
 let test1 = "Hello";
 let test2 = "Brendan";
 console.log(test1.concat(" ", test2));
