1f355a80dd0b22e2a3f60dbf9a5be38ffff74eab
MixedPepperoni <Mixyboo@gmail.com>
2025-04-09 00:45:51 -0400

    Pushing almsot full comp

--- Module5/myapp/app.js
+++ Module5/myapp/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const calculatorRoutes = require('./routes/calculatorRoutes');
+
 const app = express()
-const port = 3000
+
 // map all routes to the express app
-const calculatorRoutes =
-require('./routes/calculatorRoutes');
+
+
 app.use("/", express.static("public"));
-app.use('/calculator', calculatorRoutes);
+app.use('/calculator', calculatorRoutes); //issues was it wasnt connected to calculator. it was based on controls and add. not calcualtor.  Test in thunder first
 // export the app
 module.exports = app;
--- Module5/myapp/index.js
+++ Module5/myapp/index.js
@@ -1,14 +1,8 @@
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-// app.use(
-//  '/api-docs',
-//  swaggerUi.serve,
-//  swaggerUi.setup(swaggerDocument)
-// );
-
-// const express = require("express");
 const app = require('./app');
 const port = 3000;
+
+
+
 // import all calculator routes (up the top)
 
 
